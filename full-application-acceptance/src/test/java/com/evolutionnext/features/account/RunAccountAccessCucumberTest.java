d92ecd1305d377caf5a72d6acdf3f0913cbf76f2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2026-04-17 22:51:55 -0600

    Added Cucumber glue configuration to `RunAccountAccessCucumberTest`.

--- full-application-acceptance/src/test/java/com/evolutionnext/features/account/RunAccountAccessCucumberTest.java
+++ full-application-acceptance/src/test/java/com/evolutionnext/features/account/RunAccountAccessCucumberTest.java
@@ -1,5 +1,7 @@
 package com.evolutionnext.features.account;
 
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -7,5 +9,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/evolutionnext/features/account")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.evolutionnext.features.account")
 public class RunAccountAccessCucumberTest {
 }
